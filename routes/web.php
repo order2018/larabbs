cc4a611b2d68b02b241e2417b52a0c49e090cfaf
13480731740@qq.com <13480731740@qq.com>
2019-04-19 00:45:44 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,4 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
